94b2f0ae9cb01e0913aa85da38913064626cafdf
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-03 11:34:22 +0600

    One Setting Up

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ShantaLifeInsurance.Models;
+
+namespace ShantaLifeInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult BranchLocator()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
