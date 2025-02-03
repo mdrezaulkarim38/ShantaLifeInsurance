fe812ffd48f776867855f51fb6abb1ae05d2f576
mdrezaulkarim38 <mdrezaulkarim31295@gmail.com>
2025-02-03 16:18:35 +0600

    Now all good

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -37,6 +37,12 @@ namespace ShantaLifeInsurance.Controllers
             return View();
         }
 
+        [HttpGet()]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
